991b0996215cc132e755707dfd116d01790377ac
Eduardo David Poot Polanco <al044144@uacam.mx>
2018-04-12 13:54:34 -0500

    Chat Cliente sin webcam

---
+++ src/main/java/com/mycompany/pruebacamaramaven/webcam_test.java
@@ -0,0 +1,9 @@
+package com.mycompany.pruebacamaramaven;
+
+public class webcam_test {
+    
+    public static void main(String args[]) {
+        JFrameWindow window = new JFrameWindow();
+        window.setVisible(true);
+    }
+}
